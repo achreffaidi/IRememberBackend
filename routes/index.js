f31810087d42c76429a3e2e8347a5ca73485442b
SaidFayache <said.fayache@gmail.com>
2020-02-20 15:48:48 +0100

    fix image rotation

--- routes/index.js
+++ routes/index.js
@@ -277,7 +277,7 @@ router.post('/achref',  (req,res) => {
             Jimp.read(filePath)
                 .then( image => {
                     return image
-                        .rotate(-90)
+                        .rotate(0)
                         .write(filePath);
                 } ).catch(err => {
                     console.error(err);
